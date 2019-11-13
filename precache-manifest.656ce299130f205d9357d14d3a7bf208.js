5af1ccd0dce4c35c5389e507c337e2d05601b9bd
Navylus <sulivan.gorjao@gmail.com>
2019-11-13 15:09:08 +0100

    Updates

---
+++ precache-manifest.656ce299130f205d9357d14d3a7bf208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aeb0e36f09b8cf1b5f357c207a73c4f",
+    "url": "/One-Page-Scan/index.html"
+  },
+  {
+    "revision": "c563b332a059246ae07c",
+    "url": "/One-Page-Scan/static/css/main.adc78d68.chunk.css"
+  },
+  {
+    "revision": "5289b215743ac3173fd9",
+    "url": "/One-Page-Scan/static/js/2.c4b06cee.chunk.js"
+  },
+  {
+    "revision": "c563b332a059246ae07c",
+    "url": "/One-Page-Scan/static/js/main.021a3f39.chunk.js"
+  },
+  {
+    "revision": "5fc64b3506267713ab30",
+    "url": "/One-Page-Scan/static/js/runtime-main.fe00d33f.js"
+  }
+]);
